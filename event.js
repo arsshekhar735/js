2fa1792f379a2e3fad143bb113789c6ea30f0fd8
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-17 10:50:13 +0530

    updated

--- event.js
+++ event.js
@@ -1,2 +1,4 @@
-//If you wnat to see the event that going on the page.
+//If you want to see the event that going on the page.
 monitorEvents(document);
+//If you donot want to see the event that going on the page.
+unmonitorEvents(document);
