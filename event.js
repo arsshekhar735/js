c2c0ae005ee986535adbace8b2456d3de9c48285
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-17 12:29:47 +0530

    updated

--- event.js
+++ event.js
@@ -3,8 +3,9 @@
 // //If you donot want to see the event that going on the page.
 // unmonitorEvents(document);
 let ppara = document.querySelector('p');//here we fetch the p tag
-function changetext()
+function changetext(event)
 {
+    console.log(event);
     // let ppara = document.querySelector('p');//here we fetch the p tag
     ppara.textContent="Radhe Radhe";//here we change the content of p tag.
 }
@@ -14,4 +15,13 @@ ppara.addEventListener('click',changetext);
 
 // If you want to remove the event listener.
 
-ppara.removeEventListener('click',changetext);
+// ppara.removeEventListener('click',changetext);
+
+let anchorelement = document.querySelector('#anchor');
+
+function stopdefault(event)
+{
+    event.preventDefault();
+    anchorelement.textContent="Click Done";
+}
+anchorelement.addEventListener('click',stopdefault);
