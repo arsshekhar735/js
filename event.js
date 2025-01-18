1d073de69af4af38828f009e22588fe7dde90010
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-18 11:36:07 +0530

    updated

--- event.js
+++ event.js
@@ -1,40 +1,40 @@
-// // //If you want to see the event that going on the page.
-// // monitorEvents(document);
-// // //If you donot want to see the event that going on the page.
-// // unmonitorEvents(document);
-// let ppara = document.querySelector('p');//here we fetch the p tag
-// function changetext(event)
-// {
-//     console.log(event);
-//     // let ppara = document.querySelector('p');//here we fetch the p tag
-//     ppara.textContent="Radhe Radhe";//here we change the content of p tag.
-// }
+// //If you want to see the event that going on the page.
+// monitorEvents(document);
+// //If you donot want to see the event that going on the page.
+// unmonitorEvents(document);
+let ppara = document.querySelector('p');//here we fetch the p tag
+function changetext(event)
+{
+    console.log(event);
+    // let ppara = document.querySelector('p');//here we fetch the p tag
+    ppara.textContent="Radhe Radhe";//here we change the content of p tag.
+}
 
 
-// ppara.addEventListener('click',changetext);
+ppara.addEventListener('click',changetext);
 
-// // If you want to remove the event listener.
+// If you want to remove the event listener.
 
-// // ppara.removeEventListener('click',changetext);
-// // Selecting the anchor element using its ID 'anchor' and storing it in the 'anchorelement' variable
-// let anchorelement = document.querySelector('#anchor');
+// ppara.removeEventListener('click',changetext);
+// Selecting the anchor element using its ID 'anchor' and storing it in the 'anchorelement' variable
+let anchorelement = document.querySelector('#anchor');
 
-// // Function to stop the default behavior of the anchor link
-// function stopdefault(event) {
-//     // Prevents the default behavior of the event (e.g., preventing the page from navigating when the anchor is clicked)
-//     event.preventDefault();
+// Function to stop the default behavior of the anchor link
+function stopdefault(event) {
+    // Prevents the default behavior of the event (e.g., preventing the page from navigating when the anchor is clicked)
+    event.preventDefault();
 
-//     // Changes the text content of the anchor element to "Click Done"
-//     anchorelement.textContent = "Click Done";
-// }
+    // Changes the text content of the anchor element to "Click Done"
+    anchorelement.textContent = "Click Done";
+}
 
-// // Adding an event listener to the anchor element to listen for a 'click' event
-// // When the anchor element is clicked, the stopdefault function will be called
-// anchorelement.addEventListener('click', stopdefault);
+// Adding an event listener to the anchor element to listen for a 'click' event
+// When the anchor element is clicked, the stopdefault function will be called
+anchorelement.addEventListener('click', stopdefault);
 
 
-// //On whatever we click then para get printed
-// let paras = document.querySelectorAll('p');
+//On whatever we click then para get printed
+let paras = document.querySelectorAll('p');
 
 function tellpara(event){
     alert("You have clicked on the para: "+event.target.textContent);
