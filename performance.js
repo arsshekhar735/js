681efd51110e926c75d0d5e06ceca3929be84bd7
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-22 11:10:55 +0530

    updated

---
+++ performance.js
@@ -0,0 +1,25 @@
+// in thisa we will add the 100para in body using js.
+// for that we will write the code in method
+//code 1
+const t1 = performance.now();
+for(let i = 0; i < 100; i++)
+{
+    let para = document.createElement('p');//here p created
+    para.textContent = "This is para" + i;//p value inseterd
+    document.body.appendChild(para);//here p added into html body as child of the body tag. 
+}
+const t2 = performance.now();
+console.log("total time taken is :" + (t2-t1)); 
+
+//code 2
+const t3 = performance.now();
+let mydiv=document.createElement('div');
+for(let i = 1; i < 100; i++)
+{
+    let para = document.createElement('p');
+    para.textContent="This is para" + i;
+    mydiv.appendChild(para);
+}
+document.body.appendChild(mydiv);
+const t4 = performance.now();
+console.log("total time taken by code is :" + (t4-t3));
